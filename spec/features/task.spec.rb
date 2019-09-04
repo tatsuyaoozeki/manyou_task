30716f13fa5533d0b050e180aeee326245d8cafc
tatsuya oozeki <tasuya6281@icloud.com>
2019-09-04 19:31:05 +0900

    タスク作成のテストを作成

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -19,7 +19,23 @@ RSpec.feature "タスク管理機能", type: :feature do
   end
 
   scenario "タスク作成のテスト" do
-    
+    # new_task_pathにvisitする（タスク登録ページに遷移する）
+    # 1.ここにnew_task_pathにvisitする処理を書く
+    visit new_task_path
+    # 「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄に
+    # タスクのタイトルと内容をそれぞれfill_in（入力）する
+    # 2.ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
+    # 3.ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
+    fill_in 'Name', with: 'Example Name'
+    fill_in 'Content', with: 'Example content'
+    # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
+    # 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
+    click_on 'Create Task'
+    # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
+    # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
+    # 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
+    expect(page).to have_content 'Example Name'
+    expect(page).to have_content 'Example content'
   end
 
   scenario "タスク詳細のテスト" do
