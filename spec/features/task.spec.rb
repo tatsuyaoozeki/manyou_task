2df80c39a3916521f46125d5e6ccd9d6683727fc
tatsuya oozeki <tasuya6281@icloud.com>
2019-09-04 19:30:00 +0900

    タスク一覧のテスト作成

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -5,7 +5,17 @@ require 'rails_helper'
 RSpec.feature "タスク管理機能", type: :feature do
   # scenario（itのalias）の中に、確認したい各項目のテストの処理を書きます。
   scenario "タスク一覧のテスト" do
+    # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
+    Task.create!(title: 'test_task_01', content: 'testtesttest')
+    Task.create!(title: 'test_task_02', content: 'samplesample')
 
+    # tasks_pathにvisitする（タスク一覧ページに遷移する）
+    visit tasks_path
+
+    # visitした（到着した）expect(page)に（タスク一覧ページに）「testtesttest」「samplesample」という文字列が
+    # have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）テストを書いている
+    expect(page).to have_content 'testtesttest'
+    expect(page).to have_content 'samplesample'
   end
 
   scenario "タスク作成のテスト" do
