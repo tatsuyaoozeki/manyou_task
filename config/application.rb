7a3e6f7d1c75f5f88c304a6a0991df0dbfd23161
tatsuya oozeki <tasuya6281@icloud.com>
2019-09-06 12:43:20 +0900

    追加:application.rbにタイムゾーンを東京に指定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module ManyouTask
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.generators do |g|
