7fc7a334b51b145da790ea920e48c7997c897cde
tatsuya oozeki <tasuya6281@icloud.com>
2019-09-06 16:34:18 +0900

    追加:viewファイル・controllerに降順の指定、tasks/ja.ymlにcreated_at:作成日時、task.spec.rbにテスト作成

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -15,8 +15,8 @@ RSpec.feature "タスク管理機能", type: :feature do
 
     # visitした（到着した）expect(page)に（タスク一覧ページに）「testtesttest」「samplesample」という文字列が
     # have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）テストを書いている
-    expect(page).to have_content 'postpostpost'
-    expect(page).to have_content 'sample2sample2'
+    expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
+    expect(page).to have_content 'Factoryで作ったデフォルトのコンテント１'
   end
 
   scenario "タスク作成のテスト" do
@@ -27,16 +27,17 @@ RSpec.feature "タスク管理機能", type: :feature do
     # タスクのタイトルと内容をそれぞれfill_in（入力）する
     # 2.ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
     # 3.ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-    fill_in 'Name', with: 'Example Name'
-    fill_in 'Content', with: 'Example content'
+    fill_in 'タスク名', with: 'Factoryで作ったデフォルトのタイトル１'
+
+    fill_in 'タスク詳細', with: 'Factoryで作ったデフォルトのコンテント１'
     # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
     # 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
-    click_on 'Create Task'
+    click_on '登録する'
     # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
     # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
     # 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
-    expect(page).to have_content 'Example Name'
-    expect(page).to have_content 'Example content'
+    expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
+    expect(page).to have_content 'Factoryで作ったデフォルトのコンテント１'
   end
 
   scenario "タスク詳細のテスト" do
@@ -50,6 +51,7 @@ RSpec.feature "タスク管理機能", type: :feature do
     save_and_open_page
   end
 
-  scinario "タスクが作成日時の降順に並んでいるかのテスト" do
+  scenario "タスクが作成日時の降順に並んでいるかのテスト" do
+    
   end
 end
