f581d7f4c7836caf12b03889e70127439cc917de
tatsuya oozeki <tasuya6281@icloud.com>
2019-09-06 13:24:22 +0900

    追加:task.spec.rbにbackgroundを使ってデータを共通化

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -4,18 +4,14 @@ require 'rails_helper'
 # このRSpec.featureの右側に、「タスク管理機能」のように、テスト項目の名称を書きます（do ~ endでグループ化されています）
 RSpec.feature "タスク管理機能", type: :feature do
   # scenario（itのalias）の中に、確認したい各項目のテストの処理を書きます。
-  scenario "タスク一覧のテスト" do
+  background do
     # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
-    Task.create!(name: 'test_task_01', content: 'postpostpost')
-    Task.create!(name: 'test_task_02', content: 'sample2sample2')
-
+    Task.create!(title: 'test_task_01', content: 'testtesttest')
+    Task.create!(title: 'test_task_02', content: 'mofmofmofmof')
+  end
+  scenario "タスク一覧のテスト" do
     # tasks_pathにvisitする（タスク一覧ページに遷移する）
     visit tasks_path
-
-    # 実際の状況を確認したい箇所にさし挟む。
-    # 例の場合、「タスクが保存された後、タスク一覧ページに行くとどうなるのか」を確認するため
-    # visit tasks_path の直後に save_and_open_page を挟んでいる
-
 
     # visitした（到着した）expect(page)に（タスク一覧ページに）「testtesttest」「samplesample」という文字列が
     # have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）テストを書いている
@@ -44,15 +40,16 @@ RSpec.feature "タスク管理機能", type: :feature do
   end
 
   scenario "タスク詳細のテスト" do
-    # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
     Task.create!(name: 'test_task_03', content: 'test1test1test1')
+
     visit tasks_path
     page.first("#show").click
 
-    # visitした（到着した）expect(page)に（タスク一覧ページに）「testtesttest」「samplesample」という文字列が
-    # have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）テストを書いている
     expect(page).to have_content 'test_task_03'
     expect(page).to have_content 'test1test1test1'
     save_and_open_page
+  end
+
+  scinario "タスクが作成日時の降順に並んでいるかのテスト" do
   end
 end
