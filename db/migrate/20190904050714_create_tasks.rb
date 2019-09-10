c6342d8f3e9431195b9ee1c24f09a5ece7c396b3
tatsuya oozeki <tasuya6281@icloud.com>
2019-09-10 20:27:16 +0900

    追加:migration file　<---- datetime null:false, controllerに終了期限ソートできるように条件分岐記載、view fileにdatetimeを表示できるように記載

--- db/migrate/20190904050714_create_tasks.rb
+++ db/migrate/20190904050714_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
     create_table :tasks do |t|
       t.string :name
       t.string :content
-
+      t.datetime :deadline, null: false
       t.timestamps
     end
   end
