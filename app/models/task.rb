b5b6cc09c084a115858b969c2f5e55225d2c1ca8
tatsuya oozeki <tasuya6281@icloud.com>
2019-09-16 13:30:10 +0900

    追加:優先度を登録できるようにする・優先度で検索できるようにする

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,9 +2,10 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :content, presence: true
   enum status: { '未着手': 0, '作業中': 1, '完了': 2}
-  enum priority: { 'high': 0, 'medium': 1, 'low': 2}
+  enum priority: { high: 0, medium: 1, low: 2}
 
-  scope :task, -> (params) {where('(name LIKE ?) AND (status::text LIKE ?)',
+  scope :task, -> (params) {where('(name LIKE ?) AND (status::text LIKE ?) AND (priority::text LIKE ?)',
                                   "%#{params[:name]}%",
-                                  "%#{params[:status]}%")}
+                                  "%#{params[:status]}%",
+                                  "%#{params[:priority]}%")}
 end
