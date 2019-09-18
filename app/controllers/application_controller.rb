33c045f5d7123582b9a5f2aa9eccb99934b62f38
tatsuya oozeki <tasuya6281@icloud.com>
2019-09-18 17:35:55 +0900

    Add: ログインフォームに無効な値が入れられた時の処理と有効な値が入れられた時の処理を記載

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base
   # before_action :basic_auth
-  # protect_from_forgery with: :exception
-  #
-  # private
-
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |name, password|
-  #     name == ENV["BASIC_AUTH_NAME"] && password == ENV["BASIC_AUTH_PASSWORD"]
-  #   end
-  # end
+  protect_from_forgery with: :exception
+  include SessionsHelper
 end
