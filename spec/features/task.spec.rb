48e4335a9acd20071dda38a492859e82ff096013
tatsuya oozeki <tasuya6281@icloud.com>
2019-09-18 15:15:36 +0900

    Add: User table name/email/password_digest null:false,User and Task association

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -3,41 +3,27 @@ require 'rails_helper'
 
 # このRSpec.featureの右側に、「タスク管理機能」のように、テスト項目の名称を書きます（do ~ endでグループ化されています）
 RSpec.feature "タスク管理機能", type: :feature do
-  # scenario（itのalias）の中に、確認したい各項目のテストの処理を書きます。
+  
   background do
-    # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
     FactoryBot.create(:task)
     FactoryBot.create(:second_task)
     FactoryBot.create(:third_task)
   end
   scenario "タスク一覧のテスト" do
-    # tasks_pathにvisitする（タスク一覧ページに遷移する）
     visit tasks_path
 
-    # visitした（到着した）expect(page)に（タスク一覧ページに）「testtesttest」「samplesample」という文字列が
-    # have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）テストを書いている
     expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
     expect(page).to have_content 'Factoryで作ったデフォルトのコンテント１'
   end
 
   scenario "タスク作成のテスト" do
-    # new_task_pathにvisitする（タスク登録ページに遷移する）
-    # 1.ここにnew_task_pathにvisitする処理を書く
     visit new_task_path
-    # 「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄に
-    # タスクのタイトルと内容をそれぞれfill_in（入力）する
-    # 2.ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-    # 3.ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-    fill_in 'タスク名', with: 'Factoryで作ったデフォルトのタイトル１'
 
+    fill_in 'タスク名', with: 'Factoryで作ったデフォルトのタイトル１'
     fill_in 'タスク詳細', with: 'Factoryで作ったデフォルトのコンテント１'
 
-    # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
-    # 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
     click_on '登録する'
-    # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
-    # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
-    # 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
+
     expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
     expect(page).to have_content 'Factoryで作ったデフォルトのコンテント１'
   end
