3759cbd564fcaae49d61bf7e20cba9d742472c33
tatsuya oozeki <tasuya6281@icloud.com>
2019-09-18 16:07:12 +0900

    Add: Userモデルにセキュアパスワードを設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ApplicationRecord
   validates :content, presence: true
   enum status: { yet: 0, working: 1, completed: 2}
   enum priority: { high: 0, medium: 1, low: 2}
+  
 
   scope :task, -> (params) {where('(name LIKE ?) AND (status::text LIKE ?)',
                                   "%#{params[:name]}%",
