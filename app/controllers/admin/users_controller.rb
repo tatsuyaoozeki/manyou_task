532969ede78978c23286fa6c2d71531db83ecaac
tatsuya oozeki <tasuya6281@icloud.com>
2019-09-20 14:16:23 +0900

    Add: admin::user_controller <--- 新規作成時のcreateアクションを設定

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,6 +8,15 @@ class Admin::UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_user_path, notice: 'ユーザー｢#{@user.name}」を登録しました'
+    else
+      render :new
+    end
+  end
+
   def edit
   end
 
