59ef9b014b02001559d6cf43aefe9dea6289a5d0
tatsuya oozeki <tasuya6281@icloud.com>
2019-09-27 12:05:28 +0900

    Add: rails db:migrate:reset, rails db:seedを行う

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,7 +28,6 @@ class TasksController < ApplicationController
 
   def create
     @task = current_user.tasks.build(task_params)
-    binding.pry
     if @task.save
       redirect_to tasks_path, notice: t('.success')
     else
